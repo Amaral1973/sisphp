4e6bb45a3174f6463a3a8c7bde74fb55e360c925
Amaral1973 <61523977+Amaral1973@users.noreply.github.com>
2021-06-08 19:58:55 -0300

    Atualização dos Arquivos - 08/06/2021
    Arquivos atualizados após a aula.

--- cadpessoa.php
+++ cadpessoa.php
@@ -1,3 +1,12 @@
 <?php
-    echo 'Outra página';
+    $pessoa = array("nome" => "", "datanascimento" => "", "celular" => "", "sexo" => "");
+
+    $nome = $_POST['nome'];
+    $datanascimento = $_POST['datanascimento'];
+    $celular = $_POST['celular'];
+    $sexo = $_POST['sexo'];
+
+    array_push($pessoa, $pessoa['nome']=$nome, $pessoa['datanascimento']=$datanascimento, $pessoa['celular']=$celular, $pessoa['sexo']=$sexo);
+
+    print_r($pessoa);
 ?>
